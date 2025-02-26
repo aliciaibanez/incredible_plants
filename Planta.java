4071fbfa68e0f530c780c4984207953c9e794fae
Alicia <alicia.ibanez.escudero@gmail.com>
2025-02-26 20:29:58 +0100

    Incluye el constructor de la primera clase

--- Planta.java
+++ Planta.java
@@ -2,4 +2,10 @@
     double altura;
     String nombre;
     Cultivo tipoCultivo;
+    
+    public Planta () {
+        altura = 1.6;
+        nombre = "Cactus";
+        Cultivo tipoCultivo = new Cultivo();
+    }
 }
