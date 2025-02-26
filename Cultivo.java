1df9d47b343153e3f048f34824e80dce8db1caa6
adrian <adrian@programa.com>
2025-02-26 09:25:05 +0100

    Incluye los métodos getter y setter para cada uno de los dos atributos de la segunda clase.

--- Cultivo.java
+++ Cultivo.java
@@ -7,4 +7,20 @@ public class Cultivo
         esSecano = true;
         horasSol = 8; 
     }
+    
+    public boolean getEsSecano(){
+        return esSecano;
+    }
+    
+    public void setEsSecano(boolean seraSecano){
+        esSecano = seraSecano;
+    }
+    
+    public int getHorasSol(){
+        return horasSol;
+    }
+    
+    public void setHorasSol( int numeroHorasSol){
+        horasSol = numeroHorasSol;
+    }
 } 
